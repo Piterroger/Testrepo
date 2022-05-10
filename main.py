452604f71349372543f1493534f064eb1856f7b8
PiotrPrzyborowski <piotr.przyborowski@rogerpublishing.pl>
2022-05-10 23:57:29 +0200

    dodałem operatory

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-number1 = 3
-number2 = 4
-print(number1 + number2)
+number_1 = 3
+number_2 = 4
+suma = number_1 + number_2
+print(suma)
+
+roznica = number_1 - number_2
+print(roznica)
+
+mnozenie = number_1 * number_2
+print(mnozenie)
+
+dzielenie = number_1 / number_2
+print(dzielenie)
 
 
 
