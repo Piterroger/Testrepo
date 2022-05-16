af2bcf6d734c93565c862dbd73fadd3215a0dd2f
PiotrPrzyborowski <piotr.przyborowski@rogerpublishing.pl>
2022-05-16 20:37:26 +0200

    dodałem wiadomość

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ print(mnozenie1)
 dzielenie1 = number_1 / number_2
 print(dzielenie1)
 
+
+
 number_3 = 0.1
 number_4 = 8.2
 
@@ -26,6 +28,61 @@ print(mnozenie2)
 
 dzielenie2 = number_3 / number_4
 print(dzielenie2)
+
+
+
+liczba1 = 10
+
+liczba2 = 0
+
+dzielenieprzezzero = liczba1 / liczba2
+
+print(dzielenieprzezzero)
+
+# wykonałem dzielenie przez zero, takie działanie zawsze daje wynik 0
+
+
+
+
+number5 = 2
+
+number6 = 5
+
+potegowanie = number5 ** number6
+
+print(potegowanie)
+
+
+
+jakasliczba = 0
+
+if jakasliczba >= 0:
+print("Jestem")
+
+if jakasliczba = 5:
+print("Jestem")
+
+# wykonałem warunek który nie spełniał wymogów
+
+jakasliczba = 0
+
+if jakasliczba = 5:
+print("Liczba jest równa zeru")
+
+else:
+print("Liczba jest równa zeru")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
